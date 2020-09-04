fb72293a64cafd57d8cd6b932be540a202e470f2
jevans63 <jevans63@kent.edu>
2020-09-03 22:26:07 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// const DISPLAY = document.querySelector(".display");
+// const DIGIT7 = document.querySelector(".digit .seven");
+
+// function displayUpdate(x){
+//     DISPLAY.innerHTML = x;
+// }
+
+// DIGIT7.onclick = displayUpdate(7);
