2589b39d397e61cc825cbfbd4dfe781a05d1106c
jevans63 <jevans63@kent.edu>
2020-12-30 12:03:36 -0500

    adding addition operator functionality

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const display = document.querySelector(".display");
-var num = " ";
+var num = null;
+var temp;
+var oper;
 function displayUpdate(value){
-    num += value;
+    if(num == null)
+    num = parseInt(value);
+    else num += parseInt(value);
+    display.innerHTML = num;
+}
+function operator(value){
+    temp = parseInt(display.innerHTML);
+    if(value == '+') oper = value;
+    num = null;
+    display.innerHTML = oper;
+}
+function equals(){
+    num =  parseInt(display.innerHTML);
+    if(oper == '+') num += temp;
     display.innerHTML = num;
 }
